299d31cfcd70ba6d376e0e8c9b3836cbe99fe2d6
guibear0 <151740671+guibear0@users.noreply.github.com>
2025-08-14 19:33:41 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/api/ping', function () {
+Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
 });
 
